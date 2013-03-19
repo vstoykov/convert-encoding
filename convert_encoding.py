858aa1acf1beeba245eaaa35b2383c80b499a0f9
Venelin Stoykov <vkstoykov@gmail.com>
2013-03-19 22:22:46 +0000

    Add one more failsafe character and add comand line shortcuts
    Add short versions for "--input-encoding" and "--output-encoding
    "

--- convert_encoding.py
+++ convert_encoding.py
@@ -12,7 +12,7 @@ import sys
 import os
 from optparse import make_option, OptionParser
 
-__version__ = (0, 2)
+__version__ = (0, 3)
 
 DEFAULT_INPUT_ENCODING = 'windows-1251'
 DEFAULT_OUTPUT_ENCODING = 'iso-8859-5'
@@ -21,7 +21,9 @@ FAILSAFE_CHARACTERS = {
     u'\u2122': 'TM',
     u'\u201c': '"',
     u'\u201d': '"',
+    u'\u2026': '...',
 }
+
 
 def get_version():
     return '.'.join(unicode(x) for x in __version__)
@@ -32,7 +34,7 @@ def get_failsafe_char(char, output_encoding):
 
 
 def convert_to(in_file_name, input_encoding=DEFAULT_INPUT_ENCODING,
-                                output_encoding=DEFAULT_OUTPUT_ENCODING):
+               output_encoding=DEFAULT_OUTPUT_ENCODING):
     out_file_name = "%s.%s%s" % (in_file_name[:-4],
                                  output_encoding, in_file_name[-4:])
 
@@ -48,7 +50,7 @@ def convert_to(in_file_name, input_encoding=DEFAULT_INPUT_ENCODING,
                     new_content.append(char.encode(output_encoding))
                 except Exception as ex:
                     new_content.append(get_failsafe_char(char, output_encoding))
-                
+
             with open(out_file_name, 'w') as out_file:
                 for char in new_content:
                     out_file.write(char)
@@ -62,16 +64,17 @@ def main(*args, **options):
 if __name__ == '__main__':
     prog_name = os.path.basename(sys.argv[0])
     opt_parser = OptionParser(
-            prog=prog_name,
-            version=get_version(),
-            option_list=(
-                    make_option('--input-encoding', dest='input_encoding',
-                                default=DEFAULT_INPUT_ENCODING,
-                                help="Encoding on the input file"),
-                    make_option('--output-encoding', dest='output_encoding',
-                                default=DEFAULT_OUTPUT_ENCODING,
-                                help="Encoding on the output file"),
-                )
+        prog=prog_name,
+        version="%prog " + get_version(),
+        usage='usage: %prog [options] file1 [[file2] ... [fileN]]',
+        option_list=(
+            make_option('-i', '--input-encoding', dest='input_encoding',
+                        default=DEFAULT_INPUT_ENCODING,
+                        help="Encoding on the input file"),
+            make_option('-o', '--output-encoding', dest='output_encoding',
+                        default=DEFAULT_OUTPUT_ENCODING,
+                        help="Encoding on the output file"),
         )
+    )
     options, args = opt_parser.parse_args(sys.argv[1:])
     main(*args, **options.__dict__)
